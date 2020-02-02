98a5f689a9df8c78095773958ac07a589ee92488
ghanshyam.baviskar <ghanshyam.baviskar@netcore.co.in>
2020-02-02 21:31:19 +0530

    installing vim

--- servers/bootstrap.sh
+++ servers/bootstrap.sh
@@ -6,4 +6,4 @@ sed -i "s/%wheel\tALL=(ALL)\tALL/# %wheel\tALL=(ALL)\tALL/g" /etc/sudoers
 sed -i "s/# %wheel\tALL=(ALL)\tNOPASSWD: ALL/%wheel\tALL=(ALL)\tNOPASSWD: ALL/g" /etc/sudoers
 
 service sshd restart
-yum -y install wget telnet net-tools ping
+yum -y install wget telnet net-tools ping vim
